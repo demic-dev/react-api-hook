00ef72ca783d7ae3816532619eb55346d9de39af
demic <decillismicheledeveloper@gmail.com>
2021-04-30 19:42:45 +0200

    completed

--- README.md
+++ README.md
@@ -1,1 +1,70 @@
 # react-api-hook
+
+
+This package was created to simplify the handling of REST calls in React.
+
+To use this package, simply declare it in the component, inserting the endpoint and possible configurations (headers, body, etc.) and call it with the appropriate sendRequest function.
+
+The state of the call can be read from the state variable.
+
+Finally, to cancel the request, instead, it is sufficient to call the cancelRequest function.
+
+The state of the call has the following values:
+* _idle_
+* _loading_
+* _success_
+* _failure_
+* _inAbort_
+* _canceled_
+
+In case it is _success_ or _failure_, it will also have the properties
+_data_ and _error_ respectively.
+
+
+
+| state | type |  description |
+|--|--|--|
+| idle | string | The hook is ready to send a request. |
+| loading | string | The hook sent the request, is waiting for response. |
+| success | string | The hook succesfully received the response. Is ready to send another request. |
+| failure | string | The hook catches an error. Is ready to send another request. |
+| inAbort | string | The hook is canceling the request. |
+| canceled | string | The hook canceled the request. Is ready to send another request. |
+| data | Response | The received response. You can parse using json() function |
+| error | any | Information about the catched error. |
+
+Example:  
+
+    import React,  {  useEffect  }  from  "react";
+    import useAPIHook from  "react-api-hook";
+
+    function  App()  {
+	    const  [status,  send,  cancel]  =  useAPIHook(
+		    "https://jsonplaceholder.typicode.com/posts",
+		    {  method:  "GET"  },
+	    );
+	    
+	    useEffect(()  =>  {
+		    if (status.status  ===  "success") {
+			    console.log(status.data);
+			    //status.data.json() for the body of response  
+		    }
+		    if(status.status  ===  "failure") {
+			    console.log(status.error);
+		    }
+	    }, [status]);
+	    
+	    return (
+		    <div  className="App">
+				<header  className="App-header">
+					<div>{status.status}</div>
+				    <div>
+					    <button  onClick={send}>start fetching</button>
+					    <button  onClick={cancel}>stop fetching</button>
+				    </div>
+				</header>
+		    </div>
+	    );
+    }
+    
+    export  default  App;
---
+++ hook/index.tsx
@@ -0,0 +1,117 @@
+import { useReducer, useEffect, useRef } from "react";
+
+type ActionType = {
+  type: "fetch" | "resolve" | "reject" | "cancel" | "canceled";
+  payload?: {
+    resolve?: Response;
+    reject?: any;
+  };
+};
+
+// Return state type
+type StateType =
+  | {
+      status: "idle";
+    }
+  | {
+      status: "loading";
+    }
+  | {
+      status: "success";
+      data: Response;
+    }
+  | {
+      status: "failure";
+      error: any;
+    }
+  | {
+      status: "inAbort";
+    }
+  | {
+      status: "canceled";
+    };
+
+function fetchReducer(state: StateType, action: ActionType): StateType {
+  switch (action.type) {
+    case "fetch":
+      if (state.status !== "loading")
+        return {
+          status: "loading",
+        };
+      else return state;
+    case "resolve":
+      return {
+        status: "success",
+        data: action.payload!.resolve!,
+      };
+    case "reject":
+      return {
+        status: "failure",
+        error: action.payload!.reject,
+      };
+    case "cancel":
+      if (state.status === "loading")
+        return {
+          status: "inAbort",
+        };
+      else return state;
+    case "canceled":
+      return {
+        status: "canceled",
+      };
+    default:
+      return state;
+  }
+}
+
+/**
+ * Whit this hook, you fetch data from the endpoint and it return the state and the returned data. Return the error if there is an error.
+ * @param url
+ * @param options fetch options, such as headers, parameters, method, etc.
+ * @returns returns an array with the state and the functions for start and cancel a request.
+ */
+function useAPIHook(
+  url: RequestInfo,
+  options?: RequestInit
+): [StateType, () => void, () => void] {
+  const [status, dispatch] = useReducer<
+    (state: StateType, action: ActionType) => StateType
+  >(fetchReducer, {
+    status: "idle",
+  });
+  const controllerRef = useRef<AbortController | null>();
+
+  useEffect(() => {
+    if (status.status === "inAbort") {
+      controllerRef.current!.abort();
+    }
+    if (status.status === "loading") {
+      const controller = new AbortController();
+      controllerRef.current = controller;
+      fetch(url, { ...options, signal: controllerRef.current.signal })
+        .then((result) =>
+          dispatch({
+            type: "resolve",
+            payload: { resolve: result },
+          })
+        )
+        .catch((result) => {
+          if (controllerRef.current!.signal.aborted)
+            dispatch({ type: "canceled" });
+          else
+            dispatch({
+              type: "reject",
+              payload: { reject: result },
+            });
+        });
+    }
+  }, [status.status, url, options]);
+
+  const sendRequest = () => dispatch({ type: "fetch" });
+
+  const cancelRequest = () => dispatch({ type: "cancel" });
+
+  return [status, sendRequest, cancelRequest];
+}
+
+export default useAPIHook;
